79bf2c9211803eeecbd3b30a552e624aacfa45aa
IT-Daniel <128705123+IT-Daniel@users.noreply.github.com>
2023-04-14 09:15:39 +0200

    Add files via upload

---
+++ logoffIdleSessions.ps1
@@ -0,0 +1,44 @@
+﻿<#
+
+.SYNOPSIS
+    Log off disconnected local sessions that are inactive for more than 30 minutes.
+
+#>
+
+if (query user | Select-String "Desc") {
+
+    foreach($linea in @(query user | Select-String "Desc") -split "\n") {
+
+        $usuario = $linea -split '\s+'
+
+        #$Nombre = $usuario[1]
+        $ID = $usuario[2]
+        $Idle = $usuario[4]
+
+        if ($Idle -gt 30{
+
+            logoff $ID
+
+        }
+
+    }
+
+} elseif (query user | Select-String "Disc"){
+    
+    foreach($linea in @(query user | Select-String "Disc") -split "\n") {
+
+        $usuario = $linea -split '\s+'
+
+        #$Nombre = $usuario[1]
+        $ID = $usuario[2]
+        $Idle = $usuario[4]
+
+        if ($Idle -gt 30{
+
+            logoff $ID
+
+        }
+
+    }
+
+}
